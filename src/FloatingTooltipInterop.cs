2990fcdea8d5e87f1725d2dba481f3d8fa33b399
Jake Soenneker <jake@soenneker.com>
2025-12-21 09:06:17 -0600

    Deps

--- src/FloatingTooltipInterop.cs
+++ src/FloatingTooltipInterop.cs
@@ -2,10 +2,10 @@ using Microsoft.JSInterop;
 using Soenneker.Blazor.Floating.Tooltips.Abstract;
 using Soenneker.Blazor.Floating.Tooltips.Options;
 using Soenneker.Blazor.Utils.ResourceLoader.Abstract;
-using Soenneker.Utils.AsyncSingleton;
 using Soenneker.Utils.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Soenneker.Asyncs.Initializers;
 
 namespace Soenneker.Blazor.Floating.Tooltips;
 
@@ -13,7 +13,7 @@ namespace Soenneker.Blazor.Floating.Tooltips;
 public sealed class FloatingTooltipInterop : IFloatingTooltipInterop
 {
     private readonly IResourceLoader _resourceLoader;
-    private readonly AsyncSingleton _scriptInitializer;
+    private readonly AsyncInitializer<bool> _scriptInitializer;
     private readonly IJSRuntime _jSRuntime;
 
     private const string _module = "Soenneker.Blazor.Floating.Tooltips/js/floatingtooltipinterop.js";
@@ -24,46 +24,38 @@ public sealed class FloatingTooltipInterop : IFloatingTooltipInterop
         _jSRuntime = jSRuntime;
         _resourceLoader = resourceLoader;
 
-        _scriptInitializer = new AsyncSingleton(async (token, arg) =>
+        _scriptInitializer = new AsyncInitializer<bool>(async (useCdn, token) =>
         {
-            var useCdn = (bool) arg[0];
-
             if (useCdn)
             {
                 await _resourceLoader.LoadScriptAndWaitForVariable("https://cdn.jsdelivr.net/npm/@floating-ui/core@1.7.2/dist/floating-ui.core.umd.min.js",
-                                         "FloatingUICore", "sha256-OhWDdFHrIg8eNZaNgWL2ax7tjKNFOBQq3WErqxfHdlQ=", cancellationToken: token)
-                                     ;
+                    "FloatingUICore", "sha256-OhWDdFHrIg8eNZaNgWL2ax7tjKNFOBQq3WErqxfHdlQ=", cancellationToken: token);
 
                 await _resourceLoader.LoadScriptAndWaitForVariable("https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.7.2/dist/floating-ui.dom.umd.min.js",
-                                         "FloatingUIDOM", "sha256-cycZmidLw+l9uWDr4bUhL26YMJg1G6aM0AnUEPG9sME=", cancellationToken: token)
-                                     ;
+                    "FloatingUIDOM", "sha256-cycZmidLw+l9uWDr4bUhL26YMJg1G6aM0AnUEPG9sME=", cancellationToken: token);
             }
             else
             {
                 await _resourceLoader.LoadScriptAndWaitForVariable("_content/Soenneker.Blazor.Floating.Tooltips/js/floating-ui.core.umd.min.js",
-                                         "FloatingUIDOM", cancellationToken: token)
-                                     ;
+                    "FloatingUIDOM", cancellationToken: token);
 
                 await _resourceLoader.LoadScriptAndWaitForVariable("_content/Soenneker.Blazor.Floating.Tooltips/js/floating-ui.dom.umd.min.js", "FloatingUIDOM",
-                                         cancellationToken: token)
-                                     ;
+                    cancellationToken: token);
             }
 
             await _resourceLoader.LoadStyle("_content/Soenneker.Blazor.Floating.Tooltips/css/floatingtooltip.css", cancellationToken: token);
             await _resourceLoader.ImportModuleAndWaitUntilAvailable(_module, _moduleName, 100, token);
-
-            return new object();
         });
     }
 
     public ValueTask Initialize(bool useCdn = true, CancellationToken cancellationToken = default)
     {
-        return _scriptInitializer.Init(cancellationToken, useCdn);
+        return _scriptInitializer.Init(useCdn, cancellationToken);
     }
 
     public async ValueTask Create(string id, FloatingTooltipOptions options, CancellationToken cancellationToken = default)
     {
-        await _scriptInitializer.Init(cancellationToken, options.UseCdn);
+        await _scriptInitializer.Init(options.UseCdn, cancellationToken);
 
         string json = JsonUtil.Serialize(options)!;
 
