746d896c761f334c5d3ff051623f502ca312a424
Jake Soenneker <jake@soenneker.com>
2026-04-04 08:23:00 -0500

    quark

--- test/Soenneker.Blazor.Floating.Tooltips.Demo/Program.cs
+++ test/Soenneker.Blazor.Floating.Tooltips.Demo/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.JSInterop;
 using Serilog;
 using Serilog.Debugging;
 using Soenneker.Blazor.Floating.Tooltips.Registrars;
+using Soenneker.Quark.Gen.Lucide.Generated;
 using Soenneker.Serilog.Sinks.Browser.Blazor.Registrars;
 
 namespace Soenneker.Blazor.Floating.Tooltips.Demo;
@@ -34,6 +35,7 @@ public class Program
 
             builder.Services.AddFloatingTooltipAsScoped();
             builder.Services.AddQuarkSuiteAsScoped();
+            builder.Services.AddLucideIconsAsScoped();
 
             WebAssemblyHost host = builder.Build();
 
